73064a3062d231c5b0b9ede487e146af876a4226
CrazyCatZhang <CrazyCatZhang@outlook.com>
2023-08-14 16:50:10 +0800

    Configure Svg Icons

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -5,6 +5,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 //@ts-ignore
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'virtual:svg-icons-register'
 
 const app = createApp(App)
 app.use(ElementPlus, {
