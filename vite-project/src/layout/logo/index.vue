642a635d943735a03164ffcec7ed1ca9497c2a3d
CrazyCatZhang <CrazyCatZhang@outlook.com>
2023-08-21 15:20:41 +0800

    Logo Component Encapsulation

---
+++ vite-project/src/layout/logo/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import setting from '@/setting.ts'
+</script>
+
+<template>
+    <div class="logo">
+        <img :src="setting.logo" v-if="!setting.isLogoHidden"  alt='logo'/>
+        <p>{{ setting.title }}</p>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.logo {
+    width: 100%;
+    height: $base-menu-logo-height;
+    color: white;
+    display: flex;
+    align-items: center;
+    padding: 10px;
+
+    img {
+        width: 40px;
+        height: 40px;
+    }
+
+    p {
+        font-size: $base-logo-title-fontSize;
+        margin-left: 10px;
+    }
+}
+</style>
