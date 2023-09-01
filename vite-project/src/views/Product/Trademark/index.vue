e897f61efd972fc4b1357b520338013b56f00f8a
CrazyCatZhang <CrazyCatZhang@outlook.com>
2023-09-01 21:38:24 +0800

    Brand management data presentation

--- vite-project/src/views/Product/Trademark/index.vue
+++ vite-project/src/views/Product/Trademark/index.vue
@@ -1,26 +1,66 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { getTrademarkData } from '@/api/product/trademark'
 
 let pageNo = ref<number>(1)
 
 let limit = ref<number>(3)
 
-let total = ref<number>(400)
+let total = ref<number>(0)
+
+let trademarkArray = ref<any>([])
+
+const getTrademark = async () => {
+    const result: any = await getTrademarkData(pageNo.value, limit.value)
+    console.log(result)
+    if (result.code === 200) {
+        trademarkArray.value = result.data.records
+        total.value = result.data.total
+    }
+}
+
+onMounted(() => {
+    getTrademark()
+})
 </script>
 
 <template>
     <el-card class="box-card">
         <el-button type="primary" size="default" icon="Plus">Primary</el-button>
-        <el-table style="margin: 10px 0" border>
+        <el-table style="margin: 10px 0" border :data="trademarkArray">
             <el-table-column
                 label="序号"
                 width="80px"
                 align="center"
                 type="index"
             ></el-table-column>
-            <el-table-column label="品牌名称" align="center"></el-table-column>
-            <el-table-column label="品牌LOGO" align="center"></el-table-column>
-            <el-table-column label="品牌操作" align="center"></el-table-column>
+            <el-table-column
+                label="品牌名称"
+                align="center"
+                prop="tmName"
+            ></el-table-column>
+            <el-table-column label="品牌LOGO" align="center">
+                <template #="{ row }">
+                    <img
+                        v-if="!row.logoUrl.includes('http')"
+                        :src="'http://'+row.logoUrl"
+                        style="width: 150px; height: 100px"
+                        alt="logo"
+                    />
+                    <img
+                        v-else
+                        :src="row.logoUrl"
+                        style="width: 100px; height: 100px;background: transparent"
+                        alt="logo"
+                    />
+                </template>
+            </el-table-column>
+            <el-table-column label="品牌操作" align="center">
+                <template #='{row}'>
+                    <el-button icon='Edit' type='primary' size='small'></el-button>
+                    <el-button icon='Delete' type='danger' size='small'></el-button>
+                </template>
+            </el-table-column>
         </el-table>
         <el-pagination
             :pager-count="9"
